d890f52e5dfb52021d9048dcb3337f7b131de19a
Daniel <danielzhong124@gmail.com>
2026-06-05 10:37:37 -0400

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use('/public', express.static('./public'));
+app.use('/scripts', express.static('./public/scripts'));
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send('<h1 style="text-align: center; margin-top: 3rem;">MTG Commander Precon API</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server listening on http://localhost:${PORT}`);
+});
